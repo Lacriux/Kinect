8507de2070920f36f4b9cd6aa0d42048a0df3e07
Juanpa <juanpandradeg@gmail.com>
2018-01-22 17:54:46 -0600

    Trabajo del 22 de enero de 2018 Se agregan las instrucciones de uso, las versiones disponibles y se mejora el uso del ejecutable
    Tambien se termina de implementar la camara rgb completando la version 2.6

---
+++ Codigo/Kinect2.7/kinect_drumming/kinect.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+case "$(pidof jackdbus | wc -w)" in
+
+0)  echo "Running jack server:"
+    sudo jack_control start
+    ;;
+1)  echo "Jack server already running"
+    ;;
+esac
+
+
+sudo jack_control ds alsa
+sudo amsynth &
+sudo qjackctl &
+
+
+
+case "$(pidof a2jmidid | wc -w)" in
+
+0)  echo "Running a2jmidid:"
+    sudo a2jmidid &
+    ;;
+1)  echo "a2jmidid already running"
+    ;;
+esac
+
+sleep 2
+
+
+sudo jack_connect "amsynth:midi_in" "a2j:Midi Through [14] (capture): Midi Through Port-0"
+
+/home/electrizarte/Kinect/Kinect2.5/kinect_drumming/kinect_drumming.py
+
